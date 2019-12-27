be950109b8d21021c1b23a941c2ad6d895162ba5
Moises <elnaufrago2009@gmail.com>
2019-12-26 23:24:17 -0500

    estructura

---
+++ view/layout/footer_main.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/plugins/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/plugins/dist/js/demo.js"></script>
+</body>
+</html>
---
+++ view/login/index.php
@@ -0,0 +1,59 @@
+<?php include "../layout/header_login.php" ?>
+
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Lineysoft</b>.com</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Iniciar Session</p>
+
+      <form action="../../index3.html" method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Recordar Contrasenia
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <p class="mb-1">
+        <a href="forgot-password.html">Olvide mi contrasenia</a>
+      </p>
+      <p class="mb-0">
+        <a href="register.html" class="text-center">Registrarme</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<?php include '../layout/footer_login.php' ?>
+
