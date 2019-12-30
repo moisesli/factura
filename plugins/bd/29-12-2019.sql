f89fc53ac5dc6afca845022391746025c757e6eb
Moises <elnaufrago2009@gmail.com>
2019-12-29 21:50:33 -0500

    base datos

--- plugins/bd/29-12-2019.sql
+++ plugins/bd/29-12-2019.sql
@@ -11,13 +11,42 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Volcando datos para la tabla factura.empresas: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `empresas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `empresas` ENABLE KEYS */;
 
--- Volcando datos para la tabla factura.usuarios: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+-- Volcando estructura de base de datos para factura
+DROP DATABASE IF EXISTS `factura`;
+CREATE DATABASE IF NOT EXISTS `factura` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `factura`;
+
+-- Volcando estructura para tabla factura.empresas
+DROP TABLE IF EXISTS `empresas`;
+CREATE TABLE IF NOT EXISTS `empresas` (
+  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `ruc` varchar(11) NOT NULL,
+  `razon_social` varchar(200) NOT NULL,
+  `telefono` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `logo` varchar(100) DEFAULT NULL,
+  `produccion` varchar(2) NOT NULL DEFAULT 'no',
+  `activo` varchar(2) NOT NULL DEFAULT 'si',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla factura.usuarios
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL COMMENT 'el email es unico para empresa, si un usuario pertenece a dos empresas tendra que registrarse otro email para la otra empresa',
+  `password` varchar(100) NOT NULL,
+  `activo` varchar(2) NOT NULL DEFAULT 'si' COMMENT 'por defecto el usuario esta activo pero puede darse de baja a no',
+  `rol` varchar(50) NOT NULL DEFAULT 'usuario' COMMENT 'el rol normal es usuario pero puede ser admin u otros',
+  `telefonos` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
