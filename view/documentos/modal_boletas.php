7a94bfcb88b81b660f76318eff67a7807c3ea54f
moises <moises@iMac-de-moises.local>
2020-02-13 19:33:47 -0500

    base

--- view/documentos/modal_boletas.php
+++ view/documentos/modal_boletas.php
@@ -1,6 +1,6 @@
 <!-- Modal Boletas -->
 <div class="modal fade" id="boletaModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="staticBackdropLabel">Nueva Boleta</h5>
