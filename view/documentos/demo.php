5990b699238cca9f7b6ae958791dab9c6d2717a6
moises <elnaufrago2009@gmail.com>
2020-02-28 03:11:44 -0500

    base

---
+++ view/documentos/demo.php
@@ -0,0 +1,99 @@
+<?php
+
+use Greenter\Model\Client\Client;
+use Greenter\Model\Company\Company;
+use Greenter\Model\Company\Address;
+use Greenter\Model\Sale\Invoice;
+use Greenter\Model\Sale\SaleDetail;
+use Greenter\Model\Sale\Legend;
+
+include "../../vendor/autoload.php";
+use Greenter\Ws\Services\SunatEndpoints;
+use Greenter\See;
+
+// CLAVE SOL utilizada.
+// Ruc: 20000000001
+// Usuario: MODDATOS
+// Contraseña: moddatos
+
+$see = new See();
+$see->setService(SunatEndpoints::FE_BETA);
+$see->setCertificate(file_get_contents(__DIR__.'/../../plugins/bd/certificate.pem'));
+$see->setCredentials('20000000001MODDATOS'/*ruc+usuario*/, 'moddatos');
+
+return $see;
+
+
+// Cliente
+$client = new Client();
+$client->setTipoDoc('6')
+  ->setNumDoc('20000000001')
+  ->setRznSocial('EMPRESA 1');
+
+// Emisor
+$address = new Address();
+$address->setUbigueo('150101')
+  ->setDepartamento('LIMA')
+  ->setProvincia('LIMA')
+  ->setDistrito('LIMA')
+  ->setUrbanizacion('NONE')
+  ->setDireccion('AV LS');
+
+$company = new Company();
+$company->setRuc('20000000001')
+  ->setRazonSocial('EMPRESA SAC')
+  ->setNombreComercial('EMPRESA')
+  ->setAddress($address);
+
+// Venta
+$invoice = (new Invoice())
+  ->setUblVersion('2.1')
+  ->setTipoOperacion('0101') // Catalog. 51
+  ->setTipoDoc('01')
+  ->setSerie('F001')
+  ->setCorrelativo('1')
+  ->setFechaEmision(new DateTime())
+  ->setTipoMoneda('PEN')
+  ->setClient($client)
+  ->setMtoOperGravadas(100.00)
+  ->setMtoIGV(18.00)
+  ->setTotalImpuestos(18.00)
+  ->setValorVenta(100.00)
+  ->setSubTotal(118.00)
+  ->setMtoImpVenta(118.00)
+  ->setCompany($company);
+
+$item = (new SaleDetail())
+  ->setCodProducto('P001')
+  ->setUnidad('NIU')
+  ->setCantidad(2)
+  ->setDescripcion('PRODUCTO 1')
+  ->setMtoBaseIgv(100)
+  ->setPorcentajeIgv(18.00) // 18%
+  ->setIgv(18.00)
+  ->setTipAfeIgv('10')
+  ->setTotalImpuestos(18.00)
+  ->setMtoValorVenta(100.00)
+  ->setMtoValorUnitario(50.00)
+  ->setMtoPrecioUnitario(59.00);
+
+$legend = (new Legend())
+  ->setCode('1000')
+  ->setValue('SON DOSCIENTOS TREINTA Y SEIS CON 00/100 SOLES');
+
+$invoice->setDetails([$item])
+  ->setLegends([$legend]);
+
+$result = $see->send($invoice);
+
+// Guardar XML
+file_put_contents($invoice->getName().'.xml',
+  $see->getFactory()->getLastXml());
+if (!$result->isSuccess()) {
+  var_dump($result->getError());
+  exit();
+}
+
+echo $result->getCdrResponse()->getDescription();
+// Guardar CDR
+file_put_contents('R-'.$invoice->getName().'.zip', $result->getCdrZip());
