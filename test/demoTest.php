c6e585ab9a6bb53e75eda7e31f89273cdea6613a
moises <elnaufrago2009@gmail.com>
2020-03-07 22:58:42 -0500

    bd update

--- test/demoTest.php
+++ test/demoTest.php
@@ -4,5 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class DemoTest extends TestCase
 {
-
+    public function testPushAndPop()
+    {
+        require "factura_class.php";
+        $this->assertEquals('1','1');
+    }
 }
+    
